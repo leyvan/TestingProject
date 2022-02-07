c9fea78e04b9fdf9e244f5cd9ea9845e1b8a7200
leyvan <97409178+leyvan@users.noreply.github.com>
2022-02-07 14:47:23 -0500

    Test
    Testing

---
+++ Assets/CountDown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public int timeRemaining;
+    public Text countDownMsg;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        countDownMsg.text = "";
+        countDownMsg.gameObject.SetActive(true);
+        StartCoroutine(CountDownTimer());
+    }
+    
+    IEnumerator CountDownTimer()
+    {
+        while(timeRemaining > 0)
+        {
+            countDownMsg.text = timeRemaining.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            timeRemaining--;
+        }
+
+        countDownMsg.text = "Play!";
+
+        yield return new WaitForSeconds(1f);
+
+        countDownMsg.gameObject.SetActive(false);
+
+
+    }
+
+
+}
---
+++ Assets/EndOfBattleScreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndOfBattleScreen : MonoBehaviour
+{
+    public float playerHealth;
+    public float enemyHealth;
+
+    public GameObject endScreen;
+    public Text endText;
+
+
+    void Start()
+    {
+        endScreen.SetActive(false);
+    }
+    // Update is called once per frame
+    
+    public void OnDeath()
+    {
+            
+        endScreen.SetActive(true);
+        Debug.Log("Display that he Lost, Change Scene");
+        endText.text = "You Have Lost!";
+    }
+
+    private void BattleWon()
+    {
+        endScreen.SetActive(true);
+        Debug.Log("Display that he Won, Dancing Animation ");
+        endText.text = "You Have Won!";
+    }
+
+
+    public void KillPlayer()
+    {
+        playerHealth = 0;
+        OnDeath();
+    }
+
+    public void KillEnemy()
+    {
+        enemyHealth = 0;
+        BattleWon();
+    }
+
+
+    public void ReturnToWorldMap()
+    {
+        SceneManager.LoadScene("WorldMap");
+    }
+
+
+
+}
